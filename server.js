afb89ae7bcb7a2d6ffc864506df56c33bc6342f6
ragadeep3 <30619934+ragadeep3@users.noreply.github.com>
2017-08-04 16:57:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-   res.send(path.join(__dirname, 'ui', 'article-one.html'));
+   res.send('Article one requested and will be served here');
 });
 app.get('/article-two', function (req, res) {
-    res.send(path.join(__dirname, 'ui', 'article-two.html'));
+    res.send('Article two requested and will be served here');
 });
 app.get('/article-three', function (req, res) {
-    res.send(path.join(__dirname, 'ui', 'article-three.html'));
+    res.send('Article three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
