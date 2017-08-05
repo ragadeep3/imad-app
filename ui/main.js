892cd3022fad1aeac7af3446c7a7a5011dfeb79f
ragadeep3 <30619934+ragadeep3@users.noreply.github.com>
2017-08-05 17:15:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML = ' New value';
 
+// move the image
+var img = document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginleft = ' 100px ';
+};
